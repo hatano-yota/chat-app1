ee3da64f0a05f75d0b3d8bff756cf5baf17faa12
unreact-hatano <mizutama.hahaha@gmail.com>
2023-04-07 09:12:40 +0900

    認証状態を監視する
    onAuthStateChangedを使って認証状態を監視する

---
+++ src/feature/auth/provider/AuthProvider.tsx
@@ -0,0 +1,32 @@
+import { ReactNode, createContext, useEffect, useState } from 'react'
+import { User, getAuth, onAuthStateChanged } from '@firebase/auth'
+
+export type GlobalAuthState = {
+  user: User | null | undefined
+}
+const initialState: GlobalAuthState = {
+  user: undefined,
+}
+const AuthContext = createContext<GlobalAuthState>(initialState)
+
+type Props = { children: ReactNode }
+
+export const AuthProvider = ({ children }: Props) => {
+  const [user, setUser] = useState<GlobalAuthState>(initialState)
+
+  useEffect(() => {
+    try {
+      const auth = getAuth()
+      return onAuthStateChanged(auth, (user) => {
+        setUser({
+          user,
+        })
+      })
+    } catch (error) {
+      setUser(initialState)
+      throw error
+    }
+  }, [])
+
+  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>
+}
