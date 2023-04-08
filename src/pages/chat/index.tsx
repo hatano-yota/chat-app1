430046ca7f31174fafff336cdb3ab82ea98e4c64
unreact-hatano <mizutama.hahaha@gmail.com>
2023-04-08 19:35:52 +0900

    チャットの送信機能を実装
    const db = getDatabase()を使いfirebase.databaseのインスタンスを取得
    取得したdatabaseをref(db, 'path')で紐付ける
    pushまたはsetでデータを書き込む

---
+++ src/pages/chat/index.tsx
@@ -0,0 +1,44 @@
+import { FormEvent, useState } from 'react'
+import { FirebaseError } from 'firebase/app'
+import { getDatabase, push, ref } from '@firebase/database'
+import {
+  Button,
+  Container,
+  Heading,
+  Input,
+  Spacer,
+  chakra,
+} from '@chakra-ui/react'
+
+const Page = () => {
+  const [message, setMessage] = useState<string>('')
+
+  const handleSendMessage = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    try {
+      const db = getDatabase()
+      const dbRef = ref(db, 'chat')
+      await push(dbRef, {
+        message,
+      })
+      setMessage('')
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        console.log(e)
+      }
+    }
+  }
+
+  return (
+    <Container py={14}>
+      <Heading>チャット</Heading>
+      <Spacer height={8} aria-hidden />
+      <chakra.form display={'flex'} gap={2} onSubmit={handleSendMessage}>
+        <Input value={message} onChange={(e) => setMessage(e.target.value)} />
+        <Button type={'submit'}>送信</Button>
+      </chakra.form>
+    </Container>
+  )
+}
+
+export default Page
