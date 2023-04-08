d5e155dcbc736a85ffbc91ffbc6219b62e403824
unreact-hatano <mizutama.hahaha@gmail.com>
2023-04-08 23:42:48 +0900

    チャット取得機能の実装

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,14 +1,35 @@
-import { FormEvent, useState } from 'react'
+import { FormEvent, useState, useEffect } from 'react'
 import { FirebaseError } from 'firebase/app'
-import { getDatabase, push, ref } from '@firebase/database'
+import { getDatabase, onChildAdded, push, ref } from '@firebase/database'
 import {
+  Avatar,
+  Box,
   Button,
   Container,
+  Flex,
   Heading,
   Input,
   Spacer,
+  Text,
   chakra,
 } from '@chakra-ui/react'
+
+type MessageProps = {
+  message: string
+}
+
+const Message = ({ message }: MessageProps) => {
+  return (
+    <Flex alignItems={'start'}>
+      <Avatar />
+      <Box ml={2}>
+        <Text bgColor={'gray.200'} rounded={'md'} px={2} py={1}>
+          {message}
+        </Text>
+      </Box>
+    </Flex>
+  )
+}
 
 const Page = () => {
   const [message, setMessage] = useState<string>('')
@@ -29,10 +50,35 @@ const Page = () => {
     }
   }
 
+  const [chats, setChats] = useState<MessageProps[]>([])
+
+  useEffect(() => {
+    try {
+      const db = getDatabase()
+      const dbRef = ref(db, 'chat')
+      return onChildAdded(dbRef, (snapshot) => {
+        const message = String(snapshot.val()['message'] ?? '')
+        setChats((prev) => [...prev, { message }])
+        console.log(chats)
+      })
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        console.error(e)
+      }
+      return
+    }
+  }, [])
+
   return (
     <Container py={14}>
       <Heading>チャット</Heading>
-      <Spacer height={8} aria-hidden />
+      <Spacer height={4} aria-hidden />
+      <Flex flexDirection={'column'} overflowY={'auto'} gap={2} height={600}>
+        {chats.map((chat, index) => (
+          <Message message={chat.message} key={`ChatMessage_${index}`} />
+        ))}
+      </Flex>
+      <Spacer height={2} aria-hidden />
       <chakra.form display={'flex'} gap={2} onSubmit={handleSendMessage}>
         <Input value={message} onChange={(e) => setMessage(e.target.value)} />
         <Button type={'submit'}>送信</Button>
