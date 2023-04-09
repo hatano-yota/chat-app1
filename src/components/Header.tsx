e61d2be912e091546bb4fd58aa4bda8628032107
unreact-hatano <mizutama.hahaha@gmail.com>
2023-04-09 11:19:12 +0900

    Headerを整える

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,29 @@
-import { useState } from 'react'
 import { useRouter } from 'next/router'
 import { FirebaseError } from 'firebase/app'
 import { getAuth, signOut } from 'firebase/auth'
 import { useAuthContext } from '@src/feature/auth/provider/AuthProvider'
-import { Button, Container, Heading, chakra, useToast } from '@chakra-ui/react'
+import {
+  Avatar,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Spacer,
+  chakra,
+  useToast,
+} from '@chakra-ui/react'
+import Link from 'next/link'
 
 export const Header = () => {
   const { user } = useAuthContext()
   const { push } = useRouter()
   const toast = useToast()
-  const [isLoading, setIsLoading] = useState<boolean>(false)
 
   const handleSignOut = async () => {
-    setIsLoading(true)
     try {
       const auth = getAuth()
       await signOut(auth)
@@ -26,27 +37,36 @@ export const Header = () => {
       if (e instanceof FirebaseError) {
         console.log(e)
       }
-    } finally {
-      setIsLoading(false)
     }
   }
 
   return (
     <chakra.header py={4} bgColor={'blue.500'}>
       <Container maxW={'container.lg'}>
-        <Heading color={'white'}>
+        <Flex>
+          <Link href={'/'} passHref>
+            <chakra.a _hover={{ opacity: 0.8 }}>
+              <Heading color={'white'}>Firebase Realtime Chat</Heading>
+            </chakra.a>
+          </Link>
+          <Spacer aria-hidden />
           {user ? (
-            <Button
-              colorScheme={'teal'}
-              onClick={handleSignOut}
-              isLoading={isLoading}
-            >
-              サインアウト
-            </Button>
+            <Menu>
+              <MenuButton>
+                <Avatar flexShrink={0} width={10} height={10} />
+              </MenuButton>
+              <MenuList py={0}>
+                <MenuItem onClick={handleSignOut}>サインアウト</MenuItem>
+              </MenuList>
+            </Menu>
           ) : (
-            'ログアウト中'
+            <Link href={'/signin'} passHref>
+              <Button colorScheme={'teal'} as={'a'}>
+                サインイン
+              </Button>
+            </Link>
           )}
-        </Heading>
+        </Flex>
       </Container>
     </chakra.header>
   )
